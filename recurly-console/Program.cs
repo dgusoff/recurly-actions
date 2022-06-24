5c97aee2969f94ea42c72173ec4a2810f44924ff
Derek Gusoff <dgusoff1@gmail.com>
2022-06-24 16:28:52 -0400

    set up configuration

--- recurly-console/Program.cs
+++ recurly-console/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using recurly_console;
+
 Console.WriteLine("Hello, World!");
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((hostingContext, configuration) =>
+    {
+        configuration.Sources.Clear();
+
+        IHostEnvironment env = hostingContext.HostingEnvironment;
+        Console.WriteLine($"{env.EnvironmentName}");
+
+        configuration
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+
+        IConfigurationRoot configurationRoot = configuration.Build();
+
+        Settings settings = new();
+        configurationRoot.GetSection(nameof(Settings))
+                         .Bind(settings);
+
+        Console.WriteLine($"{settings.TestString}");
+
+    })
+    .Build();
+
+Console.WriteLine("Hey there");
+
+// Application code should start here.
+
+await host.RunAsync();
+// Get values from the config given their key and their target type.
+
+
---
+++ recurly-console/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recurly_console
+{
+    public sealed class Settings
+    {
+        public int KeyOne { get; set; }
+        public bool KeyTwo { get; set; }
+        public string TestString { get; set; }
+        public NestedSettings KeyThree { get; set; } = null!;
+    }
+
+    public sealed class NestedSettings
+    {
+        public string Message { get; set; } = null!;
+    }
+}
